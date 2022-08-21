2cfc52f254b948afcd78e0503039ac01a8ed2101
MahmudK <mahmudkenway@gmail.com>
2022-08-21 10:58:24 +0430

    Check edit status

--- src/components/todos/todoItem.tsx
+++ src/components/todos/todoItem.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from '../../models/todo';
+import EditTodo from './editTodo';
 
 interface Props{
     todo : Todo,
@@ -7,18 +8,27 @@ interface Props{
 }
 
 const TodoItem : React.FC<Props> = ({ todo, deleteTodo }) => {
+
+    const [editStatus, setEditStatus] = useState<boolean>(false)
+
     return(
-        <div className="col-6 mb-2">
-            <div className="d-flex justify-content-between align-items-center border rounded p-3">
-                <div>
-                    {todo.title}
+
+        !editStatus
+            ?
+                <div className="col-6 mb-2">
+                    <div className="d-flex justify-content-between align-items-center border rounded p-3">
+                        <div>
+                            {todo.title}
+                        </div>
+                        <div>
+                            <button type="button" className="btn btn-info btn-sm">edit</button>
+                            <button type="button" className="btn btn-danger btn-sm ml-1" onClick={() => deleteTodo(todo.id)}>delete</button>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <button type="button" className="btn btn-info btn-sm">edit</button>
-                    <button type="button" className="btn btn-danger btn-sm ml-1" onClick={() => deleteTodo(todo.id)}>delete</button>
-                </div>
-            </div>
-        </div>
+            :
+                <EditTodo todo={todo} />
+            
     )
 }
 
