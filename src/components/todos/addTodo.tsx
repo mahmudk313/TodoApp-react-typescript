117bcfc10092f5344362d35e1fd7137422d349a2
MahmudK <mahmudkenway@gmail.com>
2022-08-21 09:19:07 +0430

    Create input state and used

--- src/components/todos/addTodo.tsx
+++ src/components/todos/addTodo.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTodo : React.FC = () => {
+
+    const [input , setinput] = useState<string>("");
+
     return(
         <div className="form-inline">
             <div className="form-group d-flex">
-            <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..."/>
+            <input type="text" onChange={(e) => setinput(e.target.value)} value={input} className="form-control mx-sm-3" placeholder="i want to do ..."/>
             <button className="btn btn-primary">add</button>
             </div>
         </div>
