3ceba32e83154388770d0ed46753bac810c324e9
MahmudK <mahmudkenway@gmail.com>
2022-08-21 10:59:29 +0430

    add InputHandler

--- src/components/todos/editTodo.tsx
+++ src/components/todos/editTodo.tsx
@@ -6,10 +6,18 @@ interface Props {
 }
 
 const EditTodo : React.FC<Props> = ({ todo }) => {
+
+    const [input, setInput] = useState<string>(todo.title);
+
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value)
+    }
+
     return(
         <form className="col-6 mb-2">
             <div className="d-flex justify-content-between align-items-center border rounded p-3">
                 <div>
+                    <input type="text" onChange={inputHandler} value={input} />
                 </div>
                 <div>
                     <button type="submit" className="btn btn-info btn-sm">edit</button>
